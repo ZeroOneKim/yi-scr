a0793edaa788da87298d8380930d67ee85f28e82
ZeroOneKim <Wowwow0124@naver.com>
2022-09-03 02:22:44 +0900

    Default Setting. .

---
+++ src/main/java/com/yisoccer/yiservice/YiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yisoccer.yiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yisoccer/yiservice/YiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yisoccer.yiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
