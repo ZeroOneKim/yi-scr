76dfe821821d58a16506ccd0905fee1c800869b1
ZeroOneKim <Wowwow0124@naver.com>
2022-09-12 01:50:07 +0900

    Spring Security

---
+++ src/main/java/com/yisoccer/yiservice/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.yisoccer.yiservice.config;
+
+import com.yisoccer.yiservice.entity.Account;
+import com.yisoccer.yiservice.entity.UserAccount;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/login", "/sign-up").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated();
+
+        http.formLogin()
+                .loginPage("/login").permitAll();
+        http.logout()
+                .logoutSuccessUrl("/");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    public void login(Account account) {
+        UsernamePasswordAuthenticationToken loginToken = new UsernamePasswordAuthenticationToken(
+                new UserAccount(account),
+                account.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        SecurityContextHolder.getContext().setAuthentication(loginToken);
+    }
+}
